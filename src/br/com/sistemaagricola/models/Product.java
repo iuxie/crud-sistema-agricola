eaedc326901a951e29a4ba1e12dc84c4592683f9
iuxie <iurexavier10@gmail.com>
2026-06-16 22:32:09 -0300

    feat: add supplier class and product abstract class

---
+++ src/br/com/sistemaagricola/models/Product.java
@@ -0,0 +1,95 @@
+package br.com.sistemaagricola.models;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Product {
+
+    private final UUID id;
+    private final String name;
+    private double basePrice;
+    private int quantity;
+    private final Supplier supplier;
+
+    public Product(String name, double basePrice, int quantity, Supplier supplier) {
+        validateName(name);
+        validateBasePrice(basePrice);
+        validateQuantity(quantity);
+        validateSupplier(supplier);
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.basePrice = basePrice;
+        this.quantity = quantity;
+        this.supplier = supplier;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + name + " | Preco base: " + basePrice + " | Fornecedor: " + supplier.getEnterpriseName() + " | Quantidade: " + quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name is a required field.");
+        }
+    }
+
+    private void validateBasePrice(double basePrice) {
+        if (basePrice < 0) {
+            throw new IllegalArgumentException("Base price cannot be negative.");
+        }
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative.");
+        }
+    }
+
+    private void validateSupplier(Supplier supplier) {
+        if (supplier == null) {
+            throw new IllegalArgumentException("Supplier is a required field.");
+        }
+    }
+
+}
---
+++ src/br/com/sistemaagricola/models/Supplier.java
@@ -0,0 +1,70 @@
+package br.com.sistemaagricola.models;
+
+import java.util.Objects;
+
+public class Supplier {
+    
+    private final String cnpj, enterpriseName;
+    private int deliveryTimeDays;
+
+    public Supplier(String cnpj, String enterpriseName, int deliveryTimeDays) {
+        validateCnpj(cnpj);
+        validateEnterpriseName(enterpriseName);
+        validateDeliveryTimeDays(deliveryTimeDays);
+        this.cnpj = cnpj;
+        this.enterpriseName = enterpriseName;
+        this.deliveryTimeDays = deliveryTimeDays;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public int getDeliveryTimeDays() {
+        return deliveryTimeDays;
+    }
+
+    public void setDeliveryTimeDays(int deliveryTimeDays) {
+        this.deliveryTimeDays = deliveryTimeDays;
+    }
+
+    @Override
+    public String toString() {
+        return "CNPJ: " + cnpj + " | Nome da empresa: " + enterpriseName + " | Tempo e entrega em dias: " + deliveryTimeDays;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return Objects.equals(cnpj, supplier.cnpj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cnpj);
+    }
+
+    private void validateCnpj(String cnpj) {
+        if (cnpj == null || cnpj.length() != 14) {
+            throw new IllegalArgumentException("Invalid CNPJ. A valid CNPJ must have 14 characters.");
+        }
+    }
+
+    private void validateDeliveryTimeDays(int deliveryTimeDays) {
+        if (deliveryTimeDays < 0) {
+            throw new IllegalArgumentException("Number of days cannot be negative.");
+        }
+    }
+
+    private void validateEnterpriseName(String enterpriseName) {
+        if (enterpriseName == null || enterpriseName.isBlank()) {
+            throw new IllegalArgumentException("Enterprise name is a required field.");
+        }
+    }
+
+}
