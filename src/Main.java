c1808affb439dd8f90779a050277287283832111
iuxie <iurexavier10@gmail.com>
2026-06-17 00:52:25 -0300

    feat: add controller initial feats

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import br.com.sistemaagricola.controller.MenuController;
+import br.com.sistemaagricola.services.InventoryManager;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        InventoryManager manager = new InventoryManager();
+        MenuController menuController = new MenuController(manager);
+        menuController.start();
 
     }
 }
---
+++ src/br/com/sistemaagricola/controller/MenuController.java
@@ -0,0 +1,83 @@
+package br.com.sistemaagricola.controller;
+
+import br.com.sistemaagricola.services.InventoryManager;
+
+import java.util.Scanner;
+
+public class MenuController {
+
+    private final InventoryManager manager;
+    private boolean running;
+    private Scanner input;
+
+    public MenuController(InventoryManager manager) {
+        validateManager(manager);
+        this.manager = manager;
+        this.running = true;
+        this.input = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("🌾 Bem-vindo ao AgroTech CLI 🌾");
+
+        while (running) {
+            showMenu();
+            String option = input.nextLine();
+
+            try {
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Erro inesperado: " + e.getMessage());
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("\n--- MENU PRINCIPAL ---");
+        System.out.println("1. Registar Fornecedor");
+        System.out.println("2. Adicionar Produto ao Catálogo");
+        System.out.println("3. Listar Todos os Produtos");
+        System.out.println("4. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private void processOption(String option) {
+        switch (option) {
+            case "1":
+                registerSupplier();
+                break;
+            case "2":
+                registerProduct();
+                break;
+            case "3":
+                listProducts();
+                break;
+            case "4":
+                running = false;
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                break;
+        }
+    }
+
+    private void registerSupplier() {
+
+    }
+
+    private void registerProduct() {
+
+    }
+
+    private void listProducts() {
+
+    }
+
+    private void validateManager(InventoryManager manager) {
+        if (manager == null) {
+            throw new IllegalArgumentException("Manager is a required field.");
+        }
+    }
+
+}
